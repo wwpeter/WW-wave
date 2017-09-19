b5ad11159dc1400b3f3412712003a1614650c420
WangWei <ww@52wzb.com>
2017-09-19 11:18:45 +0800

    test

---
+++ WWWave/WWWave/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  WWWave
+//
+//  Created by wangwei on 2017/9/19.
+//  Copyright © 2017年 wangwei-WW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ WWWave/WWWave/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  WWWave
+//
+//  Created by wangwei on 2017/9/19.
+//  Copyright © 2017年 wangwei-WW. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
